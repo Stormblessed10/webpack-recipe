faa165236c1de32773773ae5b79d1a5252c27612
Stormblessed <penttilinkola10@gmail.com>
2023-04-07 13:22:16 +0300

    Add babel to the project

--- src/index.js
+++ src/index.js
@@ -1,1 +1,12 @@
-console.log('hell')
+const elvenShield = {
+    leatherStrips: 2,
+    ironIngot: 1,
+    refinedMoonstone: 4
+};
+
+const elventGauntlets = {
+    ...elvenShield,
+    refinedMoonstone: 4,
+    leather: 1,
+};
+console.log(elvenShield);
---
+++ webpack.config.js
@@ -0,0 +1,31 @@
+const path = require('path');
+let mode = process.env.NODE_ENV === "production"? "production" : "development"
+module.exports = {
+
+  mode: mode,
+
+  module: {
+    rules: [
+        {
+            test: /\.js$/,
+            exclude: /node_modules/,
+            use: {
+                loader: "babel-loader",
+            },
+        },
+    ],
+  },
+
+  devtool: "source-map",
+
+  devServer: {
+    static: {
+        directory: path.join(__dirname, 'dist'),
+    },
+    compress: true,
+    port: 9000,
+    liveReload: true,
+  },
+
+  target: "web"
+};
