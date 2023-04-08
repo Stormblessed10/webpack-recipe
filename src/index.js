47692b8edd5caa797722ccd24bad0c5afb9fc163
Stormblessed <penttilinkola10@gmail.com>
2023-04-08 18:54:07 +0300

    Setting up scss and html

--- src/index.js
+++ src/index.js
@@ -1,14 +1,1 @@
 import "./styles/index.scss"
-
-const elvenShield = {
-    leatherStrips: 2,
-    ironIngot: 1,
-    refinedMoonstone: 4
-};
-
-const elventGauntlets = {
-    ...elvenShield,
-    refinedMoonstone: 4,
-    leather: 1,
-};
-console.log(elvenShield);
